33775981584e47c4d87d701767bdb79674a918e5
Ksenia Semenova <palm87@gmail.com>
2026-04-23 12:13:51 +0200

    подчищено закоммиченное

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -1,18 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { FilmsController } from './films.controller';
-// import { FilmsService } from './films.service';
-// import { FilmsRepository } from '../repository/films.repository';
-// import { Film, FilmSchema } from './schemas/film.schema';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
-//   ],
-//   controllers: [FilmsController],
-//   providers: [FilmsService, FilmsRepository],
-// })
-// export class FilmsModule {}
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilmsController } from './films.controller';
